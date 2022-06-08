7c85419165e4244a2ed8574a3b7ac27643adae5f
A1t3r <korkunov_a@list.ru>
2022-06-09 00:23:42 +0300

    run script test

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import cv2
 
+import sys
+CENTERNET_PATH = '/content/CenterNet/src/lib'
+sys.path.insert(0, CENTERNET_PATH)
+
 from src.lib.detectors.detector_factory import detector_factory
 from src.lib.opts import opts
 
-MODEL_PATH = f'./model_last.pth'
+MODEL_PATH = f'/content/drive/MyDrive/D_data/model/resdcn18_l/model_45.pth'
 TASK = 'ctdet' # or 'multi_pose' for human pose estimation
 opt = opts().init('{} --load_model {} --dataset HT --arch resdcn_18 --debug 0 --gpus 16 --input_h 410 --input_w 720'.format(TASK, MODEL_PATH).split(' '))
 print(opt)
@@ -13,11 +17,10 @@ detector = detector_factory[opt.task](opt)
 wrktm = 0
 it = 1
 supit = 0
-limits = 198
-with open('/content/temp', 'w+') as f:
-  for i in range(limits):
-    img = cv2.imread(f'/content/CenterNet/data/HT/test_HT21/{i}.jpg')
+limits = 1980
+for i in range(limits):
+    img = cv2.imread(f'/content/drive/MyDrive/D_data/HT21/HT/test_HT21/{i}.jpg')
     retres18 = detector.run(img)
     wrktm += retres18['net']
     it += 1
-print(wrktm/2871)
+print(wrktm/1980)
